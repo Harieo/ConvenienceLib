1526329d6f070163c0ed230f37e57f5ce3329645
Harieo <harieo@hotmail.com>
2021-08-27 10:44:04 +0100

    Added ClickAction to replace the player-only MenuItem click consumer

--- src/main/java/uk/co/harieo/ConvenienceLib/menus/MenuInteractionListener.java
+++ src/main/java/uk/co/harieo/ConvenienceLib/menus/MenuInteractionListener.java
@@ -5,45 +5,46 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import uk.co.harieo.ConvenienceLib.menus.click.ClickAction;
 
 public class MenuInteractionListener implements Listener {
 
-	private final MenuFactory factory;
+    private final MenuFactory factory;
 
-	/**
-	 * A generic {@link Listener} which listens for the factory's inventory being clicked then passes the event onto
-	 * any instance of {@link MenuItem} in the slot which was clicked
-	 *
-	 * @param factory to listen for
-	 */
-	public MenuInteractionListener(MenuFactory factory) {
-		this.factory = factory;
-	}
+    /**
+     * A generic {@link Listener} which listens for the factory's inventory being clicked then passes the event onto
+     * any instance of {@link MenuItem} in the slot which was clicked
+     *
+     * @param factory to listen for
+     */
+    public MenuInteractionListener(MenuFactory factory) {
+        this.factory = factory;
+    }
 
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent event) {
-		if (event.getWhoClicked() instanceof Player) {
-			Player player = (Player) event.getWhoClicked();
-			int slotClicked = event.getSlot();
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getWhoClicked() instanceof Player player) {
+            int slotClicked = event.getSlot();
 
-			Inventory clickedInventory = event.getClickedInventory();
-			if (clickedInventory == null)
-				return;
-			MenuImpl factoryMenuImpl = factory.getMenu(player);
-			if (factoryMenuImpl == null) // They may be clicking a menu from another factory
-				return;
-			Inventory factoryInventory = factoryMenuImpl.getInventory();
+            Inventory clickedInventory = event.getClickedInventory();
+            if (clickedInventory == null)
+                return;
+            MenuImpl factoryMenuImpl = factory.getMenu(player);
+            if (factoryMenuImpl == null) // They may be clicking a menu from another factory
+                return;
+            Inventory factoryInventory = factoryMenuImpl.getInventory();
 
-			if (clickedInventory.equals(factoryInventory)) {
-				event.setCancelled(true);
-				MenuItem item = factory.getItem(player, slotClicked);
-				if (item != null) {
-					item.onClick(player);
-				}
-			} else if (event.isShiftClick() && event.getView().getTopInventory().equals(factoryInventory)) {
-				event.setCancelled(true); // Stop items merging with the factory inventory
-			}
-		}
-	}
+            if (clickedInventory.equals(factoryInventory)) {
+                event.setCancelled(true);
+                MenuItem item = factory.getItem(player, slotClicked);
+                if (item != null) {
+                    ClickAction clickAction = new ClickAction(event);
+                    item.onClick(clickAction);
+                }
+            } else if (event.isShiftClick() && event.getView().getTopInventory().equals(factoryInventory)) {
+                event.setCancelled(true); // Stop items merging with the factory inventory
+            }
+        }
+    }
 
 }
