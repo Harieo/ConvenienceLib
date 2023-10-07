8bac1f87a13828f7fb8171eba902528f32beb11e
Harieo <harieo@hotmail.com>
2023-10-07 12:27:13 +0100

    Added Bungee module

---
+++ bungee/src/main/java/net/harieo/ConvenienceLib/bungee/ConvenienceLibBungee.java
@@ -0,0 +1,12 @@
+package net.harieo.ConvenienceLib.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ConvenienceLibBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+}
